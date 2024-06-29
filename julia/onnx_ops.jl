efb4700f64a6e216be17822e111cc62116e5138a
Eric Brown <eric@ebrown.net>
2024-06-28 22:21:36 -0600

    Add support for 0d arrays

--- julia/onnx_ops.jl
+++ julia/onnx_ops.jl
@@ -32,9 +32,10 @@ function ONNX.load_node!(tape::Tape, ::OpConfig{:ONNX, :Cast}, args::VarVec, att
 end
 
 function ONNX.load_node!(tape::Tape, ::OpConfig{:ONNX, :Range}, args::VarVec, attrs::AttrDict)
-    start = tape[args[1]].val[1]
-    stop = tape[args[2]].val[1] - 1
-    step = tape[args[3]].val[1]
+    start = tape[args[1]].val[1] |> only
+    stop = tape[args[2]].val[1] |> only
+    stop = stop - 1
+    step = tape[args[3]].val[1] |> only
     return push_call!(tape, collect ∘ range; start, stop, step)
 end
 
@@ -68,4 +69,54 @@ end
 
 function ONNX.load_node!(tape::Tape, ::OpConfig{:ONNX, :Div}, args::VarVec, attrs::AttrDict)
     return push_call!(tape, broadcast, /, tape[args[1]].val, tape[args[2]].val)
+end
+
+function NNlib.unsqueeze(x::AbstractArray, dims)
+    new_shape::Vector{Integer} = collect(size(x))
+    for d in sort(collect(dims))
+        insert!(new_shape, d, 1)
+    end
+    return reshape(x, new_shape...)
+end
+
+function ONNX.onnx_unsqueeze(x::Real, dims)
+    return ONNX.onnx_unsqueeze(fill(x, ()), dims)
+end
+
+function ONNX.take(
+    data::AbstractArray{T, N}, idxs::AbstractArray{Int, M};
+    dim=ndims(data)) where {T, N, M}
+    if length(idxs) == 1 && data isa ONNX.SVector
+        # we use SVector to represent array size, Gather(arr_sz, idx)
+        # works as size(arr, idx); but since dimensions are reversed,
+        # we need to reverse the index as well
+        # see https://github.com/FluxML/ONNX.jl/issues/62 for details
+        idx = length(data) .- idxs .+ 1
+        dat = data[idx] |> only
+        return fill(dat, ())
+    end
+    if length(idxs) == 1
+        # special case, works as getindex
+        # this needs to be a zero-dimensional array
+        dat = data[idxs] |> only
+        return fill(dat, ())
+    end
+    # we will take slices of data of this size
+    size_before = (size(data)[1:dim-1]...,)
+    size_after = (size(data)[dim+1:ndims(data)]...,)
+    # and put them into output array at out[:, :, ..., idxs[i, j, ...]]
+    out = similar(data, (size_before..., size(idxs)..., size_after...))
+    colons_before = [(:) for _=1:dim-1]
+    colons_after = [(:) for _=dim+1:ndims(data)]
+    # iteration over idxs doesn't depend on data or dimension
+    # we iterate over the last index purely due to memory layout
+    for i=1:size(idxs, ndims(idxs))
+        # R - slice of idxs (not slice of data!)
+        R = [[(:) for _=1:ndims(idxs)-1]..., i]
+        # ensure I = idxs[R...] is itself an array and not a scalar
+        I = [idxs[R...]...,]
+        slice = data[colons_before..., I, colons_after...]
+        out[colons_before..., R..., colons_after...] = slice
+    end
+    return out
 end
