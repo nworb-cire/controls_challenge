3f564a6a3cdbf1d7fccfbff6ac8307253622dae3
Eric Brown <eric@ebrown.net>
2024-08-07 21:14:04 -0600

    Add dataset

--- torch/data.py
+++ torch/data.py
@@ -1,17 +1,50 @@
 import os
 import urllib.request
 import zipfile
+from glob import glob
 from io import BytesIO
 
+import numpy as np
+import pandas as pd
 import pytorch_lightning as pl
+import torch
+from torch.utils.data import Dataset
 
 from tinyphysics import DATASET_PATH, DATASET_URL
 
 
-# class
+class LatAccelDataset(Dataset):
+    CONTEXT_SIZE = 20
+    SEQUENCE_LENGTH = 100
+
+    def __init__(self, data):
+        assert data.ndim == 3
+        self.data = torch.tensor(data, dtype=torch.float32)
+
+    def __len__(self):
+        return self.data.shape[0] * (self.SEQUENCE_LENGTH - self.CONTEXT_SIZE * 2)
+
+    def __getitem__(self, idx):
+        seq_idx = idx // (self.SEQUENCE_LENGTH - self.CONTEXT_SIZE * 2)
+        start_idx = idx % (self.SEQUENCE_LENGTH - self.CONTEXT_SIZE * 2)
+
+        states = self.data[seq_idx, start_idx:start_idx + self.CONTEXT_SIZE, :-1]
+        tokens = self.data[seq_idx, start_idx:start_idx + self.CONTEXT_SIZE, -1]
+        exog = self.data[seq_idx, start_idx + self.CONTEXT_SIZE:start_idx + self.CONTEXT_SIZE * 2, 1:-1]
+        target = self.data[seq_idx, start_idx + self.CONTEXT_SIZE:start_idx + self.CONTEXT_SIZE * 2, -1]
+
+        return states, tokens, exog, target
 
 
 class DataModule(pl.LightningDataModule):
+    x_cols = [
+        "steerCommand",
+        "roll",
+        "vEgo",
+        "aEgo",
+    ]
+    y_col = "targetLateralAcceleration"
+
     def prepare_data(self) -> None:
         if not DATASET_PATH.exists():
             print("Downloading dataset (0.6G)...")
@@ -23,5 +56,27 @@ class DataModule(pl.LightningDataModule):
                             with z.open(member) as src, open(DATASET_PATH / os.path.basename(member), 'wb') as dest:
                                 dest.write(src.read())
 
-    def setup(self, stage):
-        pass
+    def setup(self, stage: str = None):
+        segments = []
+        for file in glob(f"{DATASET_PATH}/*.csv"):
+            df = pd.read_csv(file)
+            df = df[self.x_cols + [self.y_col]]
+            df["roll"] = np.sin(df["roll"]) * 9.81
+            df = df.dropna()
+            # add batch dimension
+            val = df.values[np.newaxis]
+            segments.append(val)
+
+        # Concatenate: (n_sequences, sequence_length, n_features)
+        data = np.concatenate(segments, axis=0)
+
+        train_size = int(data.shape[0] * 0.9)
+        print(f"Train size: {train_size:,}, Val size: {data.shape[0] - train_size:,}")
+        self.train = LatAccelDataset(data[:train_size, :, :])
+        self.val = LatAccelDataset(data[train_size:, :, :])
+
+    def train_dataloader(self):
+        return torch.utils.data.DataLoader(self.train, batch_size=32, shuffle=True)
+
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(self.val, batch_size=32)
