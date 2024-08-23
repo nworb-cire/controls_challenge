a78d6a9c04483230c4c43c757fe50ad1dc183240
Eric Brown <eric@ebrown.net>
2024-08-22 21:02:09 -0600

    Add NN controller

---
+++ controllers/nn_train.py
@@ -0,0 +1,54 @@
+import pytorch_lightning as pl
+import torch
+from torch import nn
+
+from . import BaseController
+
+FUTURE_PLAN_LENGTH = 10
+
+
+class ControlsModel(pl.LightningModule):
+    def __init__(
+        self,
+        state_dim: int = 64,
+        hidden_dim: int = 64,
+        out_dim: int = 1,
+    ):
+        super().__init__()
+        # target lataccel, current lataccel, state, future plan lataccel
+        self.input_size = 2 + 3 + FUTURE_PLAN_LENGTH
+        self.state_dim = state_dim
+        self.network = nn.Sequential(
+            nn.Linear(self.input_size + state_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(hidden_dim, out_dim + state_dim),
+            nn.Tanh(),
+        )
+
+    def forward(self, x, st):
+        x = torch.cat([x, st], dim=-1)
+        x = self.network(x)
+        x, st = x.split([1, self.state_dim], dim=-1)
+        return x, st
+
+
+class Controller(BaseController):
+    def __init__(self):
+        self.model = ControlsModel()
+        self.state = torch.zeros((1, self.model.state_dim))
+
+    def update(self, target_lataccel: float, current_lataccel: float, state, future_plan):
+        future_plan = future_plan.lataccel[:, :FUTURE_PLAN_LENGTH]
+        future_plan = torch.cat([future_plan, torch.zeros((future_plan.shape[0], FUTURE_PLAN_LENGTH - future_plan.shape[1]))], dim=-1)
+        inp = torch.cat((
+            target_lataccel.unsqueeze(-1),
+            current_lataccel.unsqueeze(-1),
+            state,
+            future_plan
+        ), dim=-1)
+        out, self.state = self.model(inp, self.state)
+        return out[:, 0]
